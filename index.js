967327ee60737f13ea72c2bd65393af18641d5b9
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-14 14:10:22 +0100

    middleware

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -91,6 +95,8 @@ let persons = [
     persons.concat.apply(person)
     response.json(person)
   })
+
+
 
   const PORT = 3001
   app.listen(PORT, () => {
