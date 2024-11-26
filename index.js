e5b1c19339826fd65f5fef49cca85a2f6d5877f7
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-26 19:57:29 +0100

    uibuild

--- index.js
+++ index.js
@@ -99,7 +99,13 @@ app.post('/api/persons', (request, response, next) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+  .catch(error => {
+    if (error.name === 'ValidationError') {
+      response.status(400).json({ error: error.message });
+    } else {
+      next(error);
+    }
+  });
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
