fb10d5eee0348d3703039203b811319e3111bd70
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-26 17:14:26 +0100

    added put

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -63,38 +76,46 @@ app.delete('/api/persons/:id', (request, response) => {
   .then(result => {
     response.status(204).end()
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
+  .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
   })
 })
 
-  // const getRandomId = () => {
-  //   let randomNumber
-  //   do {
-  //       randomNumber = Math.floor(Math.random() * 9999)
-  //   } while (persons.some(person => person.id === randomNumber))
-  //   return String(randomNumber)
-  // }
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
 
-    if (!body.name || !body.number) {
-      return response.status(400).json({ error: 'name or number missing' })
-    }
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+	.then(updatedPerson => {
+		response.json(updatedPerson)
+	})
+	.catch(error => next(error))
+})
 
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    })
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-  })
-
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
