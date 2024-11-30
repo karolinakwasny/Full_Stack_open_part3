4d1a6d46ab0f3f3d1b6117c73f5d29c64db91d07
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-30 14:24:40 +0100

    uibuild

--- index.js
+++ index.js
@@ -26,46 +26,23 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 const Person = require('./models/person')
 
-let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-  ]
-
-  app.get('/info', (request, response, next) => {
-    Person.countDocuments()
-      .then((numberOfPersons) => {
-        const currentDate = new Date().toString()
-        response.send(`Phonebook has info for ${numberOfPersons} people <p>${currentDate}</p>`)
-      })
-      .catch(error => next(error))
-  })
-
-  app.get('/api/persons', (request, response) => {
-	  Person.find({}).then(persons => {
-      response.json(persons)
+app.get('/info', (request, response, next) => {
+  Person.countDocuments()
+    .then((numberOfPersons) => {
+      const currentDate = new Date().toString()
+      response.send(`Phonebook has info for ${numberOfPersons} people <p>${currentDate}</p>`)
     })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
       if (person) {
       response.json(person)
